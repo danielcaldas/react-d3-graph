3abca59fb74979fbcd9c038145c09a83c14acc4e
danielcaldas <danielcaldas@sapo.pt>
2017-03-29 00:29:07 +0100

    Remove unecessary config file import from graph container (Layout)

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
-import CONFIG from './graph.config';
-
 import Graph from './Graph';
 import graphMock from '../../mock';
 
 export default class Layout extends React.Component {
     render() {
-        // Customizable properties
-        let myConfig = Object.assign({}, CONFIG);
-
-        myConfig.STROKE_THICKNESS = 2;
-        myConfig.LABEL_PROPERTY = 'uid';
-        myConfig.DEFAULT_NODE_COLOR = '#ae13e4';
-
         const graphProps = {
             data: graphMock.graph,
-            config: myConfig
+            config: {
+                STROKE_THICKNESS: 2,
+                LABEL_PROPERTY: 'uid',
+                DEFAULT_NODE_COLOR: '#ae13e4'
+            }
         };
 
         return (
