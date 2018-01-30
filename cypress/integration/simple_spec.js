22e0a836ff6adb551d3238510d9c63c2c548aaa6
danielcaldas <caldasjdaniel@gmail.com>
2018-01-30 00:41:29 +0000

    Setup cypress. Add simple sample test

---
+++ cypress/integration/simple_spec.js
@@ -0,0 +1,32 @@
+describe('Smoke tests', function () {
+    it('should render graph with react-d3-graph in sandbox', function () {
+        // stub window
+        const windowSpy = cy.stub();
+
+        cy.on('window:alert', windowSpy);
+
+        // go to sandbox
+        cy.visit('http://localhost:3002');
+
+        // add +10 random nodes to the graph
+        for (let i = 0; i < 10; i++) {
+            cy.contains('+').click();
+        }
+
+        // click on the "Fullscreen" button
+        cy.contains('Fullscreen').click();
+
+        // wait 3 seconds
+        cy.wait(3000);
+
+        // pause the graph
+        cy.get('[data-reactroot=""] > :nth-child(1) > :nth-child(1) > :nth-child(3)').click();
+
+        // click on some node and verify the window.alert call
+        cy.get('#Drone > path').click().then(() => expect(windowSpy.getCall(0)).to.be.calledWith('Clicked node Drone'));
+
+        // https://docs.cypress.io/guides/references/assertions.html#Value
+        // should see 24 nodes
+        cy.get('.node').should('have.length', 24);
+    });
+});
