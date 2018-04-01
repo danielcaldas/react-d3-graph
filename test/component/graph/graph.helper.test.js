19d43e6b626ae39f35f076751ad8039ee54a44ce
danielcaldas <caldasjdaniel@gmail.com>
2018-04-01 01:58:40 +0100

    Add unit test for svg at config level

--- test/component/graph/graph.helper.test.js
+++ test/component/graph/graph.helper.test.js
@@ -24,7 +24,7 @@ describe('Graph Helper', () => {
         let that = {};
 
         beforeEach(() => {
-            const nodeConfig = Object.assign({}, config.node);
+            const nodeConfig = Object.assign({}, config.node, { svg: 'file.svg' });
             const linkConfig = Object.assign({}, config.link);
 
             that = {
@@ -70,7 +70,7 @@ describe('Graph Helper', () => {
                     size: 200,
                     stroke: 'yellow',
                     strokeWidth: 2,
-                    svg: '',
+                    svg: 'file.svg',
                     type: 'circle'
                 });
             });
@@ -111,7 +111,7 @@ describe('Graph Helper', () => {
                         size: 200,
                         stroke: 'none',
                         strokeWidth: 1.5,
-                        svg: '',
+                        svg: 'file.svg',
                         type: 'circle'
                     });
                 });
@@ -151,7 +151,7 @@ describe('Graph Helper', () => {
                         size: 200,
                         stroke: 'none',
                         strokeWidth: 1.5,
-                        svg: '',
+                        svg: 'file.svg',
                         type: 'circle'
                     });
                 });
