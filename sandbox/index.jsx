0abd9ff04297a9c0fe0f10460e2367bc03319d82
danielcaldas <caldasjdaniel@gmail.com>
2018-04-11 20:52:36 +0100

    Remove nonsense react-router-dom dep

--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom';
 
 import Sandbox from './Sandbox';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(
-    <Router>
-        <div>
-            <Route path='/' component={Sandbox}/>
-        </div>
-    </Router>
-    , app);
+ReactDOM.render(<Sandbox/>, app);
