c29e443c8763b40575127e4fce1554055d324c71
danielcaldas <caldasjdaniel@gmail.com>
2018-08-19 17:16:11 +0200

    Add marker.const

---
+++ src/components/marker/marker.const.js
@@ -0,0 +1,11 @@
+// internal marker flavours for cross referencing
+const MARKERS = {
+    MARKER_S: 'marker-small',
+    MARKER_SH: 'marker-small-highlighted',
+    MARKER_M: 'marker-medium',
+    MARKER_MH: 'marker-medium-highlighted',
+    MARKER_L: 'marker-large',
+    MARKER_LH: 'marker-large-highlighted'
+};
+
+export { MARKERS };
