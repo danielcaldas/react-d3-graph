23e3454e5e302d66dab7f51156215b74a124ddaa
Daniel Caldas <caldasjdaniel@gmail.com>
2018-12-19 22:40:18 +0000

    Fix graph.mock files

--- test/snapshot/graph/graph.mock.js
+++ test/snapshot/graph/graph.mock.js
@@ -110,142 +110,142 @@ export default {
         {
             source: 'Harry',
             target: 'Sally',
-            value: '1.2'
+            value: 1.2
         },
         {
             source: 'Harry',
             target: 'Mario',
-            value: '1.3'
+            value: 1.3
         },
         {
             source: 'Sarah',
             target: 'Alice',
-            value: '0.2'
+            value: 0.2
         },
         {
             source: 'Eveie',
             target: 'Alice',
-            value: '0.5'
+            value: 0.5
         },
         {
             source: 'Peter',
             target: 'Alice',
-            value: '1.6'
+            value: 1.6
         },
         {
             source: 'Mario',
             target: 'Alice',
-            value: '0.4'
+            value: 0.4
         },
         {
             source: 'James',
             target: 'Alice',
-            value: '0.6'
+            value: 0.6
         },
         {
             source: 'Harry',
             target: 'Carol',
-            value: '0.7'
+            value: 0.7
         },
         {
             source: 'Harry',
             target: 'Nicky',
-            value: '0.8'
+            value: 0.8
         },
         {
             source: 'Bobby',
             target: 'Frank',
-            value: '0.8'
+            value: 0.8
         },
         {
             source: 'Alice',
             target: 'Mario',
-            value: '0.7'
+            value: 0.7
         },
         {
             source: 'Harry',
             target: 'Lynne',
-            value: '0.5'
+            value: 0.5
         },
         {
             source: 'Sarah',
             target: 'James',
-            value: '1.9'
+            value: 1.9
         },
         {
             source: 'Roger',
             target: 'James',
-            value: '1.1'
+            value: 1.1
         },
         {
             source: 'Maddy',
             target: 'James',
-            value: '0.3'
+            value: 0.3
         },
         {
             source: 'Sonny',
             target: 'Roger',
-            value: '0.5'
+            value: 0.5
         },
         {
             source: 'James',
             target: 'Roger',
-            value: '1.5'
+            value: 1.5
         },
         {
             source: 'Alice',
             target: 'Peter',
-            value: '1.1'
+            value: 1.1
         },
         {
             source: 'Johan',
             target: 'Peter',
-            value: '1.6'
+            value: 1.6
         },
         {
             source: 'Alice',
             target: 'Eveie',
-            value: '0.5'
+            value: 0.5
         },
         {
             source: 'Harry',
             target: 'Eveie',
-            value: '0.1'
+            value: 0.1
         },
         {
             source: 'Eveie',
             target: 'Harry',
-            value: '2.0'
+            value: 2.0
         },
         {
             source: 'Henry',
             target: 'Mikey',
-            value: '0.4'
+            value: 0.4
         },
         {
             source: 'Elric',
             target: 'Mikey',
-            value: '0.6'
+            value: 0.6
         },
         {
             source: 'James',
             target: 'Sarah',
-            value: '1.5'
+            value: 1.5
         },
         {
             source: 'Alice',
             target: 'Sarah',
-            value: '0.6'
+            value: 0.6
         },
         {
             source: 'James',
             target: 'Maddy',
-            value: '0.5'
+            value: 0.5
         },
         {
             source: 'Peter',
             target: 'Johan',
-            value: '0.7'
+            value: 0.7
         }
     ]
 };
