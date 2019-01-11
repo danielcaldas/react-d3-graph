b12ef202f83cfda2713b2c1d782a88c59da265ab
Daniel Caldas <caldasjdaniel@gmail.com>
2019-01-11 23:36:32 +0000

    Change custom-node sample to have 0 valued id

--- sandbox/data/custom-node/custom-node.data.js
+++ sandbox/data/custom-node/custom-node.data.js
@@ -1,50 +1,50 @@
 module.exports = {
     links: [
         {
-            source: 1,
-            target: 2
+            source: 0,
+            target: 2,
         },
         {
-            source: 1,
-            target: 3
+            source: 0,
+            target: 3,
         },
         {
-            source: 1,
-            target: 4
+            source: 0,
+            target: 4,
         },
         {
             source: 3,
-            target: 4
-        }
+            target: 4,
+        },
     ],
     nodes: [
         {
-            id: 1,
-            name: 'Mary',
-            gender: 'female',
+            id: 0,
+            name: "Mary",
+            gender: "female",
             hasCar: false,
-            hasBike: false
+            hasBike: false,
         },
         {
             id: 2,
-            name: 'Roy',
-            gender: 'male',
+            name: "Roy",
+            gender: "male",
             hasCar: false,
-            hasBike: true
+            hasBike: true,
         },
         {
             id: 3,
-            name: 'Frank',
-            gender: 'male',
+            name: "Frank",
+            gender: "male",
             hasCar: true,
-            hasBike: true
+            hasBike: true,
         },
         {
             id: 4,
-            name: 'Melanie',
-            gender: 'female',
+            name: "Melanie",
+            gender: "female",
             hasCar: true,
-            hasBike: false
-        }
-    ]
+            hasBike: false,
+        },
+    ],
 };
