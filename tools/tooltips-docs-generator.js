48f23805c3ea182cc949151fe1384d752bcf29d7
danielcaldas <caldasjdaniel@gmail.com>
2019-10-01 17:48:24 +0200

    Strip from tooltips the blocks of javascript. Add link to live docs in every tooltip bottom

--- tools/tooltips-docs-generator.js
+++ tools/tooltips-docs-generator.js
@@ -1,6 +1,11 @@
 const data = require("./graph-config-jsdoc");
-
 const TOOLTIPS_MAX_WIDTH = 400;
+const LIVE_DOCS_LINK = "https://goodguydaniel.com/react-d3-graph/docs/index.html";
+const DOCS_LINK_MARKUP = `\
+    <small>\
+        for more details check the <a target="_blank" href="${LIVE_DOCS_LINK}">official documentation</a>\
+    </small>\
+`;
 
 if (!data || !data.length || !data[0].params) {
     throw new Error("Invalid JSON provided from jsdoc parser");
@@ -36,21 +41,31 @@ function getParamInfo(param) {
         .replace(/width="(\d+)"/gi, "width='400'")
         .replace(/height="(\d+)"/gi, "height='200'");
 
-    // TODO: extract text only info from the description field, it may contain
-    // md or html
     return {
         [name]: `\
             <h4>${name}</h4>\
             <b>type</b>: ${ftype} | <b>default value</b>: ${defaultvalue} | <b>optional</b>: ${optional}\
             <h5>Description</h5>\
             <div style="max-width: ${TOOLTIPS_MAX_WIDTH}px;">${description}</div>\
+            ${DOCS_LINK_MARKUP}\
         `,
     };
 }
 
-const graphConfigElms = data[0].params.map(getParamInfo).reduce((acc, o) => ({ ...o, ...acc }), {});
+/**
+ * Removes JavaScript markdown blocks from the input text.
+ * @param {string} s the code block from where we want
+ * to remove the code block marks.
+ * @returns {string} final text without javascript code blocks.
+ */
+function stripJsMdBlocks(s) {
+    return s.replace(/```javascript(.*)```/gi, "");
+}
 
-console.log(`\
-    /*eslint-disable*/\n\
-    export const tooltips = ${JSON.stringify(graphConfigElms, null, 2)};\
-`);
+const graphConfigElms = data[0].params.map(getParamInfo).reduce((acc, o) => ({ ...o, ...acc }), {});
+const output = `\
+/*eslint-disable*/\n\
+export const tooltips = ${JSON.stringify(graphConfigElms, null, 2)};\
+`;
+
+console.log(stripJsMdBlocks(output));
