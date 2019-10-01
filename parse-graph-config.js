4e0fd466bd3755a14c652caefa25d20253504909
danielcaldas <caldasjdaniel@gmail.com>
2019-10-01 12:51:20 +0200

    When parseing graph config for tooltips make images smaller

--- parse-graph-config.js
+++ parse-graph-config.js
@@ -27,17 +27,22 @@ if (!data || !data.length || !data[0].params) {
  * information mapped by the param name.
  */
 function getParamInfo(param) {
-    const { type, optional, defaultvalue, description, name } = param;
+    const { type, optional, defaultvalue, description: rawDescription, name } = param;
     const types = type && type.names && type.names;
     const ftype = types && types.length ? types.join("|") : "*";
+
+    // make images smaller so that they fit in the tooltip
+    const description = rawDescription
+        .replace(/width="(\d+)"/gi, "width='400'")
+        .replace(/height="(\d+)"/gi, "height='200'");
 
     // TODO: extract text only info from the description field, it may contain
     // md or html
     return {
-        [name]: `
-            <h4>${name}</h4>
+        [name]: `\
+            <h4>${name}</h4>\
             <b>type</b>: ${ftype} | <b>default value</b>: ${defaultvalue} | <b>optional</b>: ${optional}\
-            <h5>Description</h5>
+            <h5>Description</h5>\
             <div style="max-width: ${TOOLTIPS_MAX_WIDTH}px;">${description}</div>\
         `,
     };
