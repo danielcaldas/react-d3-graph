d6233eec9a91f50b14032f1ddfae5f1866a075e0
atharvajava <atharvajava@gmail.com>
2019-10-14 12:30:53 +0200

    local link type resolved as per response

--- src/components/graph/graph.builder.js
+++ src/components/graph/graph.builder.js
@@ -52,12 +52,12 @@ function _getNodeOpacity(node, highlightedNode, highlightedLink, config) {
  * @memberof Graph/builder
  */
 function buildLinkProps(link, nodes, links, config, linkCallbacks, highlightedNode, highlightedLink, transform) {
-    const { source, target, linkType } = link;
+    const { source, target } = link;
     const x1 = nodes?.[source]?.x || 0;
     const y1 = nodes?.[source]?.y || 0;
     const x2 = nodes?.[target]?.x || 0;
     const y2 = nodes?.[target]?.y || 0;
-    const type = linkType || config.link.type;
+    const type = link.type || config.link.type;
     const d = buildLinkPathDefinition({ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } }, type);
 
     let mainNodeParticipates = false;
