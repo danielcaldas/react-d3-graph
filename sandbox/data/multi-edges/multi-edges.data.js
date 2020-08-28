ac2f6786d0220e8843968f29a1b521320c1777be
danielcaldas <caldasjdaniel@gmail.com>
2020-08-28 22:29:46 +0800

    Add ids to multi-edge data

--- sandbox/data/multi-edges/multi-edges.data.js
+++ sandbox/data/multi-edges/multi-edges.data.js
@@ -1,33 +1,43 @@
 module.exports = {
     links: [
         {
+            id: "12",
             source: 1,
             target: 2,
             label: "link between 1 and 2",
+            type: "STRAIGHT",
         },
         {
+            id: "122",
             source: 1,
             target: 2,
             label: "second link between 1 and 2",
+            type: "CURVE_FULL",
         },
         {
+            id: "123",
             source: 1,
             target: 2,
             label: "third link between 1 and 2",
+            type: "CURVE_SMOOTH",
         },
         {
+            id: "13",
             source: 1,
             target: 3,
         },
         {
+            id: "14",
             source: 1,
             target: 4,
         },
         {
+            id: "34",
             source: 3,
             target: 4,
         },
         {
+            id: "341",
             source: 3,
             target: 4,
         },
