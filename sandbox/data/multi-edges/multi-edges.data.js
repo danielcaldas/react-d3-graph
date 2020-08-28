2df4cbb5685fda24e1a369f8386e413900c90d20
danielcaldas <caldasjdaniel@gmail.com>
2020-08-28 22:06:22 +0800

    Add multi-edges dataset

---
+++ sandbox/data/multi-edges/multi-edges.data.js
@@ -0,0 +1,53 @@
+module.exports = {
+    links: [
+        {
+            source: 1,
+            target: 2,
+            label: "link between 1 and 2",
+        },
+        {
+            source: 1,
+            target: 2,
+            label: "second link between 1 and 2",
+        },
+        {
+            source: 1,
+            target: 2,
+            label: "third link between 1 and 2",
+        },
+        {
+            source: 1,
+            target: 3,
+        },
+        {
+            source: 1,
+            target: 4,
+        },
+        {
+            source: 3,
+            target: 4,
+        },
+        {
+            source: 3,
+            target: 4,
+        },
+    ],
+    nodes: [
+        {
+            id: 1,
+            name: "Node 1",
+        },
+        {
+            id: 2,
+            name: "Node 2",
+        },
+        {
+            id: 3,
+            name: "Node 3",
+        },
+        {
+            id: 4,
+            name: "Node 4",
+        },
+    ],
+};
